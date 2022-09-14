573f08dd4fd82f7abb3625dc10eb0f1753df3dda
ceylann96 <91429246+ceylann96@users.noreply.github.com>
2022-09-14 15:30:30 +0300

    Learning JWT

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,7 +1,9 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,5 +18,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Onu gecemez";
         public static string ProductNameAlreadyExists = "Ürün zaten var";
         public static string CategoryLimitExceeded = "en fazla 15 kategoriden ürün ekleyebilirsiniz";
+        public static string AuthorizationDenied="yetkiniz yok";
+        public static string UserRegistered="kullanıcı kaydedildi";
+        public static string UserNotFound="kullanıcı bulunamadı";
+        public static string PasswordError="parola hatası";
+        public static string SuccessfulLogin = "başarılı giriş";
+        public static string UserAlreadyExists="kullanıcı zaten mevcut";
+        public static string AccessTokenCreated="access token oluşturuldu";
     }
 }
